9c399018f303aea25649a872c895fb77cba62034
chaerin-dev <chaerin.dev@gmail.com>
2023-01-26 14:06:43 +0900

    REFACTOR: Initialize isEditing state on refresh

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import Component from './library/Component.js';
 import Trello from './components/Trello.js';
-import { setInitialGlobalState, getGlobalState } from './library/globalState.js';
+import { setInitialGlobalState, setGlobalState, getGlobalState } from './library/globalState.js';
 
 class App extends Component {
   constructor(props) {
@@ -29,15 +29,25 @@ class App extends Component {
         type: 'beforeunload',
         selector: 'window',
         handler: () => {
-          const newList = [
-            ...getGlobalState().lists.map(list => (list.isEditingTitle ? { ...list, isEditingTitle: false } : list)),
-          ];
-          const newTrelloState = {
-            ...getGlobalState(),
-            list: newList,
-            modal: { listId: null, cardId: null, isOpened: false, isEditingTitle: false, isEditingDescription: false },
-          };
-          localStorage.setItem('trelloState', JSON.stringify(newTrelloState));
+          setGlobalState(beforeState => {
+            const newList = [
+              ...beforeState.lists.map(list => ({ ...list, isEditingTitle: false, isAddingCard: false })),
+            ];
+            const newTrelloState = {
+              ...beforeState,
+              isAddingList: false,
+              lists: newList,
+              modal: {
+                listId: null,
+                cardId: null,
+                isOpened: false,
+                isEditingTitle: false,
+                isEditingDescription: false,
+              },
+            };
+            return newTrelloState;
+          });
+          localStorage.setItem('trelloState', JSON.stringify(getGlobalState()));
         },
       },
     ];
